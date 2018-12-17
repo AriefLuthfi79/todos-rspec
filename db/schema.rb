455a061fb8965916a3d5282c4ed6f5c9b9a317a1
ariefluthfi79 <ariefluthfi799@gmail.com>
2018-12-17 21:19:01 +0700

    Adding completed todo by user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_065203) do
+ActiveRecord::Schema.define(version: 2018_12_17_134025) do
 
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.datetime "completed_at"
   end
 
 end
